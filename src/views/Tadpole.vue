7cf6b828df8864c7c16fb3f18abdba9055ab20c5
ziqin_wang <719713496@qq.com>
2022-02-14 20:19:22 +0800

    小蝌蚪

---
+++ src/views/Tadpole.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="width:100px;border: 1px solid red">
+    <h1>
+      tad--pole
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref, reactive, toRefs, watch, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import { log } from '../weapons/index'
+import { message } from 'ant-design-vue'
+
+
+export default defineComponent({
+  name: 'Tadpole',
+  setup () {
+    onMounted(() => {
+      log('This is Tad--Pole!!!')
+    })
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+</style>
--- test.js
+++ test.js
@@ -1,6 +1,25 @@
-const f = () => {
-    var x = {}
-    var y = {}
-    x.a = y
-    y.a = x
+// 数值四舍五入
+const neatPoints = (_points) => {
+    const xArr = String(_points.x).split('')
+    const yArr = String(_points.y).split('')
+    const xIndex = xArr.indexOf('.')
+    const yIndex = yArr.indexOf('.')
+    let x = Math.floor(_points.x)
+    let y = Math.floor(_points.y)
+    if (Number(xArr[xIndex + 1]) >= 5) {
+        x++
+    }
+    if (Number(yArr[yIndex + 1]) >= 5) {
+        y++
+    }
+    return {
+        x: x,
+        y: y
+    }
 }
+
+const _points = {
+    x: 100,
+    y: 10
+}
+console.log(neatPoints(_points))
